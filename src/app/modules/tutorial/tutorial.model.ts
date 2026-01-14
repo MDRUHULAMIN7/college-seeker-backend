92a3d102a8304dcd2a60b16d6b5b3fd5bb73a967
mdruhulamin7 <ruhulthisis@gmail.com>
2026-01-14 08:58:06 +0600

    trying to solve deplyement error

--- src/app/modules/tutorial/tutorial.model.ts
+++ src/app/modules/tutorial/tutorial.model.ts
@@ -5,7 +5,9 @@ import type { ITutorial } from './tutorial.interface.js';
 const tutorialSchema: Schema<ITutorial> = new Schema({
   title: { type: String, required: true, trim: true },
   description: { type: String, trim: true },
-  videoUrl: { type: String, required: true },
+
+  videoUrl:
+   { type: String, required: true },
   createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 },
   { timestamps: true } 
